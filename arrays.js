fff5f1499b35656db8b347026caac7231c476ab6
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-07-10 22:26:04 -0500

    Add index postion definition and examples

--- arrays.js
+++ arrays.js
@@ -1,23 +1,34 @@
-// Array of strings
+// Array of strings;
 
 var family = ["Justin", "Thomas", "Tinkerbell", "Mom", "Dad", "Jonathan"];
 
-//Array of numbers
+// Array of numbers;
 
 var yearsTogether = [2019, 2020, 2021, 2022];
 
-//Array of booleans
+// Array of booleans;
 
 var booleans = [true, false, true, false];
 
-//The pop method will remove 2022 from the array and return that value to us.
+// The pop method will remove "2022" from the array and return that value to us.;
 
 yearsTogether.pop;
 
-//The shift method will remove the last false from the booleans array and return the length of the remaining elements in the array.
+// The shift method will remove the last "false" from the booleans array and return the length of the remaining elements in the array.;
 
 booleans.shift (false);
 
-//The push method will add Chris to the end of the family array.
+// The push method will add "Chris" to the end of the family array.;
 
 family.push("Chris");
+
+
+/* Index position is the first element in an array. Instead of the first element in an array being considered number "1", it actually is number "0". 
+The reason being is that starting with "0" requires less processing by the computer and thus more efficient. 
+In the examples, I will demonstrate examples of index positions.;  */
+
+console.log(family [0]);        // This will print the index position which is "Justin";
+console.log (yearsTogether[2]); // This will print "2021";
+console.log (booleans [1]);     // This will print "false";
+
+
