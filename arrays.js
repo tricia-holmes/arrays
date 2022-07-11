3d51e2a26e4f0cd3c7425002bf701c2fb90a48b8
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-07-10 22:06:43 -0500

    Add arrays

---
+++ arrays.js
@@ -0,0 +1,11 @@
+// Array of strings
+
+var family = ["Justin", "Thomas", "Tinkerbell", "Mom", "Dad", "Jonathan"];
+
+//Array of numbers
+
+var yearsTogether = [2019, 2020, 2021, 2022];
+
+//Array of booleans
+
+var booleans = [true, false, true, fase];
