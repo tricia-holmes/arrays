4896efe7b74bff84d95e7f8ffb4dc3368360bc40
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-07-10 22:14:07 -0500

    Add array methods and comments

--- arrays.js
+++ arrays.js
@@ -8,4 +8,16 @@ var yearsTogether = [2019, 2020, 2021, 2022];
 
 //Array of booleans
 
-var booleans = [true, false, true, fase];
+var booleans = [true, false, true, false];
+
+//The pop method will remove 2022 from the array and return that value to us.
+
+yearsTogether.pop;
+
+//The shift method will remove the last false from the booleans array and return the length of the remaining elements in the array.
+
+booleans.shift (false);
+
+//The push method will add Chris to the end of the family array.
+
+family.push("Chris");
